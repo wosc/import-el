b94402ee6805d18f479a1d66d40e73dd52deffc9
Wolfgang Schnerring <wosc@wosc.de>
2012-12-07 17:21:20 +0100

    whitespace

--- tests/test_import.el
+++ tests/test_import.el
@@ -1,18 +1,20 @@
 (load "../wosc-import.el")
 
+
 (defun this-line ()
   (save-excursion
     (let ((start (progn (move-beginning-of-line nil) (point)))
           (end (progn (move-end-of-line nil) (point))))
-      (buffer-substring start end))
+      (buffer-substring-no-properties start end))
     )
   )
 
+
 (ert-deftest creates-import-line ()
-             (with-temp-buffer
-               (insert "import zoo\nfoo\n")
-               (_wosc-create-import "foo.bar")
-               (goto-char (point-min))
-               (should (equal "import foo.bar" (this-line)))
-               )
-)
+  (with-temp-buffer
+    (insert "import zoo\nfoo\n")
+    (_wosc-create-import "foo.bar")
+    (goto-char (point-min))
+    (should (equal "import foo.bar" (this-line)))
+    )
+  )
