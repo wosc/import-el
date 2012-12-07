88359d4c0e1030d8282af2c85f9095173f70586c
Wolfgang Schnerring <wosc@wosc.de>
2012-12-07 19:40:14 +0100

    Running tests on command line doesn't work with emacs23 for some reason

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-emacs --no-site-file -batch -L ./lib -L . -l ert -l tests/test_import.el -f ert-run-tests-batch-and-exit
+emacs24 --no-site-file -batch -L ./lib -L . -l ert -l tests/test_import.el -f ert-run-tests-batch-and-exit
