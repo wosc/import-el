9642e317f3407527fd26b738ff72cdebb4498072
Wolfgang Schnerring <ws@gocept.com>
2012-12-07 19:41:42 +0100

    Write some characterization tests, allow using create-import when there are no imports in the buffer yet

--- tests/test_import.el
+++ tests/test_import.el
@@ -10,11 +10,55 @@
   )
 
 
-(ert-deftest wosc-import-test-creates-import-line ()
+(ert-deftest wosc-import-test-creates-import-line-in-empty-buffer ()
+  (with-temp-buffer
+    (_wosc-create-import "foo.bar")
+    (goto-char (point-min))
+    (should (equal "import foo.bar" (this-line)))
+    )
+  )
+
+(ert-deftest wosc-import-test-creates-import-line-with-no-import-lines ()
+  (with-temp-buffer
+    (insert "foo\n")
+    (_wosc-create-import "foo.bar")
+    (goto-char (point-min))
+    (should (equal "import foo.bar" (this-line)))
+    )
+  )
+
+(ert-deftest wosc-import-test-creates-import-line-with-import-line ()
   (with-temp-buffer
     (insert "import zoo\nfoo\n")
     (_wosc-create-import "foo.bar")
     (goto-char (point-min))
     (should (equal "import foo.bar" (this-line)))
+    )
+  )
+
+(ert-deftest wosc-import-test-sorts-import-lines ()
+  (with-temp-buffer
+    (insert "import c\nimport a.b\nimport b\nimport a")
+    (wosc-sort-imports)
+    (should (equal "import a\nimport a.b\nimport b\nimport c"
+                   (buffer-substring (point-min) (point-max))))
+    )
+  )
+
+(ert-deftest wosc-import-test-sorts-import-lines-with-blanks-after ()
+  (with-temp-buffer
+    (insert "import c\nimport a.b\nimport b\nimport a\n\n\n\n")
+    (wosc-sort-imports)
+    (should (equal "import a\nimport a.b\nimport b\nimport c\n\n\n\n"
+                   (buffer-substring (point-min) (point-max))))
+    )
+  )
+
+(ert-deftest wosc-import-test-sorts-import-lines-with-code-after ()
+  (with-temp-buffer
+    (insert "import c\nimport a.b\nimport b\nimport a\n\nfoo.bar()")
+    (wosc-sort-imports)
+    (should (equal "import a\nimport a.b\nimport b\nimport c\n\nfoo.bar()"
+                   (buffer-substring (point-min) (point-max))))
     )
   )
